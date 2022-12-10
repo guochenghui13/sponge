53baf08839e1acaadc1fd4b1fea8e1dac832435c
guochenghui <guochenghui@fenbi.com>
2022-12-10 21:25:07 +0800

    lab0

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -12,42 +12,54 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
+ByteStream::ByteStream(const size_t capacity)
+    : _queue(), _capacity_size(capacity), _written_size(0), _read_size(0), _end_input(false), _error(false) {}
 
 size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    if (_end_input)
+        return 0;
+    size_t write_size = min(data.size(), _capacity_size - _queue.size());
+    _written_size += write_size;
+    for (size_t i = 0; i < write_size; i++)
+        _queue.push_back(data[i]);
+    return write_size;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    size_t pop_size = min(len, _queue.size());
+    return string(_queue.begin(), _queue.begin() + pop_size);
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
+void ByteStream::pop_output(const size_t len) {
+    size_t pop_size = min(len, _queue.size());
+    _read_size += len;
+    for (size_t i = 0; i < pop_size; i++)
+        _queue.pop_front();
+}
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+    string data = this->peek_output(len);
+    this->pop_output(len);
+    return data;
 }
 
-void ByteStream::end_input() {}
+void ByteStream::end_input() {_end_input = true; }
 
-bool ByteStream::input_ended() const { return {}; }
+bool ByteStream::input_ended() const { return _end_input; }
 
-size_t ByteStream::buffer_size() const { return {}; }
+size_t ByteStream::buffer_size() const { return _queue.size(); }
 
-bool ByteStream::buffer_empty() const { return {}; }
+bool ByteStream::buffer_empty() const { return _queue.empty(); }
 
-bool ByteStream::eof() const { return false; }
+bool ByteStream::eof() const { return _end_input && _queue.empty(); }
 
-size_t ByteStream::bytes_written() const { return {}; }
+size_t ByteStream::bytes_written() const { return _written_size;}
 
-size_t ByteStream::bytes_read() const { return {}; }
+size_t ByteStream::bytes_read() const { return _read_size; }
 
-size_t ByteStream::remaining_capacity() const { return {}; }
+size_t ByteStream::remaining_capacity() const { return _capacity_size - _queue.size(); }
--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -1,6 +1,7 @@
 #ifndef SPONGE_LIBSPONGE_BYTE_STREAM_HH
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
+#include <deque>
 #include <string>
 
 //! \brief An in-order byte stream.
@@ -16,6 +17,11 @@ class ByteStream {
     // all, but if any of your tests are taking longer than a second,
     // that's a sign that you probably want to keep exploring
     // different approaches.
+    std::deque<char> _queue;
+    size_t _capacity_size;
+    size_t _written_size;
+    size_t _read_size;
+    bool _end_input;
 
     bool _error{};  //!< Flag indicating that the stream suffered an error.
 
