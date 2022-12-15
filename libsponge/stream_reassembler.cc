6aaac4774f40770bccf2ab0b6cc9b17eaf428bdb
guochenghui <guochenghui@fenbi.com>
2022-12-15 16:49:04 +0800

    lab1_done

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -15,7 +15,7 @@ using namespace std;
 StreamReassembler::StreamReassembler(const size_t capacity) :
     _output(capacity),
     _capacity(capacity),
-    _unassemble_strs(),
+    _buf(),
     _first_unread(0),
     _first_unassembled(0),
     _first_unacceptable(0),
@@ -29,23 +29,22 @@ StreamReassembler::StreamReassembler(const size_t capacity) :
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
     // 首先判断是否到达eof, 记录_eof_index, 之后如果组装到了这个的前一个， 那么关闭bytestream的写
+    int len = data.size();
     if(eof) {
         _eof = true;
-        _eof_idx = index + data.size();
+        _eof_idx = index + len;
     }
-    // index < _first_unread
-    // 1. 如果index + data.size() - 1 < _first_unassembled: return
+    // TODO: index < _first_unread
+    // 1. 如果index + len - 1 < _first_unassembled: return
     // 2. 如果 >= , 截取后进行递归的调用
-    if(index < get_first_unassembled() && index + data.size() <= get_first_unassembled() ){
+
+    // TODO: index + len - first_unread >= capacity: return
+    // 这是无符号的整数， 要判断不能为负数否则会溢出
+    if(index < get_first_unassembled() && index + len <= get_first_unassembled() ){
         return;
     }
-    if(index < get_first_unassembled()  && index + data.size() > get_first_unassembled() ){
+    if(index < get_first_unassembled()  && index + len > get_first_unassembled() ){
         push_substring(data.substr(get_first_unassembled()  - index) ,get_first_unassembled() , eof);
-        return;
-    }
-    // 如果index + data.size() - first_unread >= capacity: return
-    // TODO: 这个地方不太确定是否要抛弃， 还是放在reassemble的buffer里
-    if(index + data.size() - _first_unread >= _capacity){
         return;
     }
     // 如果index == first_unassembled
@@ -62,31 +61,28 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
     } else{
         put_in_buff(data, index);
     }
-    // _eof标识位是true， 并且_eof_idx 之前已经被装配了
-    cout << get_first_unassembled()  << endl;
-    cout << _eof_idx << endl;
 
     if(_eof && get_first_unassembled()  == _eof_idx){
-        _unassemble_strs.clear();
+        _buf.clear();
         _output.end_input();
     }
 }
 
-void StreamReassembler::reassemble(const size_t _f_ua, const size_t _f_unacc){
-    // TODO: 完成加入字符串后， 看看map中还有什么能合并
+void StreamReassembler::reassemble(const size_t old_first_unassembled, const size_t old_first_unacceptable){
     string s = string();
-    for(size_t i = _f_ua; i < _f_unacc; i++){
-        // 不存在map中直接跳过
-        if(_unassemble_strs.count(i) == 0) continue;
-
-        // 已经读到bytestream就不要了
-        if(i + _unassemble_strs[i].size() - 1 < get_first_unassembled()){
-            _unassemble_strs.erase(i);
+    auto new_first_unassembled = get_first_unassembled();
+    for(size_t i = old_first_unassembled; i < old_first_unacceptable; i++){
+        if(_buf.count(i) == 0){
+            if(i >= new_first_unassembled) break;
             continue;
         }
-        if(i == _first_unassembled + s.length()){
-            s.append(_unassemble_strs[i]);
-            _unassemble_strs.erase(i);
+        if(i < new_first_unassembled){
+            _buf.erase(i);
+            continue ;
+        }
+        if(i == new_first_unassembled + s.length()){
+            s.push_back(_buf[i]);
+            _buf.erase(i);
         } else{
             break;
         }
@@ -95,43 +91,16 @@ void StreamReassembler::reassemble(const size_t _f_ua, const size_t _f_unacc){
 }
 
 void StreamReassembler::put_in_buff(const std::string &data, const size_t idx){
-    if(_unassemble_strs.size() == 0){
-        _unassemble_strs[idx] = data;
-        return;
-    }
-
-    // TODO: 主要要看看map中是否有重合的string， 如果有的话要合并
-    auto higher = _unassemble_strs.upper_bound(idx + data.size());
-    auto lower = _unassemble_strs.upper_bound(idx);
-
-    // 判断是否到头了， 可能存在没有比idx还小
-    if(lower != _unassemble_strs.begin()) lower--;
-    if(higher != _unassemble_strs.begin()) higher--;
-
-    // 看看是否和前面合并
-    if(lower->first != idx){
-        auto tmp = lower->second;
-        tmp = tmp.substr(0, idx - lower->first);
-        _unassemble_strs[lower->first] = tmp;
-    }
-
-    // 看看是否和后面合并
-    // 如果后面的结尾小于index + data.size() 不用处理了， 后面直接删掉
-    // 否则把后面的开头截取下重新放入
-    if(higher->first != idx) {
-        if (idx + data.size() < higher->first + _unassemble_strs[higher->first].size()) {
-            auto tmp = higher->second;
-            tmp = tmp.substr(idx + data.size() - higher->first + higher->second.size());
-            _unassemble_strs[idx + data.size()] = tmp;
+    int unacceptable = get_first_unacceptable();
+    for(size_t i= 0; i < data.length(); i++){
+        if(idx + i < get_first_unacceptable()){
+            _buf[idx + i] = data[i];
         }
     }
-    // 在index -> index + data.size()中的所有区间都被删去
-    _unassemble_strs.erase(_unassemble_strs.lower_bound(idx), _unassemble_strs.upper_bound(idx + data.size() - 1));
 }
 
 size_t StreamReassembler::get_first_unassembled() {return _output.bytes_written(); }
 size_t StreamReassembler::get_first_unacceptable() {return _output.bytes_read() + _capacity; }
-
-size_t StreamReassembler::unassembled_bytes() const {return _first_unassembled; }
-
-bool StreamReassembler::empty() const { return _unassemble_strs.size() == 0; }
+size_t StreamReassembler::get_first_unread() {return _output.bytes_read(); }
+size_t StreamReassembler::unassembled_bytes() const {return _buf.size(); }
+bool StreamReassembler::empty() const { return _buf.empty(); }
